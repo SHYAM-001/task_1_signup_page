dc62f87dbc5f2eb7f9e2389e2dfc3a7eb55c4af0
SHYAM K S <103324177+SHYAM-001@users.noreply.github.com>
2023-11-27 20:36:29 +0530

    Update urls.py

--- task-1/internship/hospital/urls.py
+++ task-1/internship/hospital/urls.py
@@ -3,8 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home"),
-    path("index",views.index,name="index"),
+    path("",views.index,name="index"),
     path("login",views.login_view,name="login"),
     path("logout",views.logout_view,name="logout"),
     path("register/Patient",views.register_p,name="register_p"),
